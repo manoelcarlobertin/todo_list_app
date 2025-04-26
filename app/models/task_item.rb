f33c8cccea27b37723c13c8ca3713499dff9dc58
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-04-26 18:53:59 -0300

    Ajustei alguns relacionamentos nos Modells.

--- app/models/task_item.rb
+++ app/models/task_item.rb
@@ -1,3 +1,4 @@
 class TaskItem < ApplicationRecord
-  belongs_to :task_list
+  belongs_to :task_list, optional: true, inverse_of: :task_items
+  belongs_to :user, though: :task_list, optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :task_lists, dependent: :destroy
+  has_many :items, through: :task_lists
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
